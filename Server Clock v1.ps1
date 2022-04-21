ad5f712cb4a5bcd837848d19f01071b936f88b71
r8btx <N/A>
2022-04-21 22:29:39 +0900

    Initial Upload

---
+++ Server Clock v1.ps1
@@ -0,0 +1,92 @@
+﻿# Set Target Server
+$uri = 'https://time.is'
+
+# Set Variables
+$stopwatch = New-Object System.Diagnostics.Stopwatch
+$stopwatch_internal = New-Object System.Diagnostics.Stopwatch
+$UserAgent = ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
+
+$global:server_clock = $null
+$global:sync_corrected = $false
+$sync_attempt_min = 20
+$sync_attempt_max = 30
+$sync_delay = 250
+
+function Init-Clock(){
+    $r = curl.exe -s -I -L -X HEAD $uri -A $UserAgent -w "%{time_pretransfer}\n%{time_starttransfer}\n%{time_total}"
+    if ($r.Length -le 3) {
+        Write-Host('Failed to reach',$uri)
+    } else {
+        $stopwatch.Start()
+
+        # Get "Date" in HTTP response header / Start backward in case of redirections
+        For ($i=$r.Length-1;$i -ge 0;$i--) {if ($r[$i] -match "^Date:") {$server_time = [DateTime]$r[$i].Split(',')[1]; break}}
+
+        # Get cURL (time_total - time_starttransfer) + (time_starttransfer - time_pretransfer)/2 in tick
+        $time_to_respond = [int64](([decimal]$r[-1] - [decimal]$r[-2])*10000000) + [int64](([decimal]$r[-2] - [decimal]$r[-3])*5000000)
+        
+        # Roughly compensate for the time-to-respond
+        $server_time = $server_time.AddTicks($time_to_respond)
+
+        $global:server_clock = $server_time
+    }
+}
+
+
+
+function Sync-Clock(){
+    $r = curl.exe -s -I -L -X HEAD $uri -A $UserAgent -w "%{time_pretransfer}\n%{time_starttransfer}\n%{time_total}"
+    $stopwatch_internal.Restart()
+    if ($r.Length -le 3) {
+        Write-Host('Failed to reach',$uri)
+    } else {
+        For ($i=$r.Length-1;$i -ge 0;$i--) {if ($r[$i] -match "^Date:") {$server_time = [DateTime]$r[$i].Split(',')[1]; break}}
+
+        $time_to_respond = [int64](([decimal]$r[-1] - [decimal]$r[-2])*10000000) + [int64](([decimal]$r[-2] - [decimal]$r[-3])*5000000)
+        
+        $server_time = $server_time.AddTicks($time_to_respond)
+        $expected_clock = [DateTime]($server_clock.Ticks + $stopwatch.ElapsedTicks - $stopwatch_internal.ElapsedTicks)
+        $diff =  $expected_clock.Ticks - $server_time.Ticks
+        
+        # if new time is greater than expected clock, update
+        # if expected clock is greater than server time by more than 1 sec, update
+        if ($diff -lt 0) {
+            $stopwatch.Restart()
+            $global:sync_corrected = $true
+            $global:server_clock = [DateTime]($server_time.Ticks + $stopwatch_internal.ElapsedTicks)
+        } elseif ($diff -ge 10000000){
+            $stopwatch.Restart()
+            $global:server_clock = [DateTime]($server_time.Ticks + $stopwatch_internal.ElapsedTicks)
+        }
+    }
+    $stopwatch_internal.Stop()    
+}
+
+
+# Resize Console Window
+$pswindow = (Get-Host).ui.rawui
+$newsize = $pswindow.windowsize
+$newsize.height = 5
+$newsize.width = 30
+$pswindow.windowsize = $newsize
+$newsize = $pswindow.buffersize
+$newsize.height = 5
+$newsize.width = 30
+$pswindow.buffersize = $newsize
+
+
+Init-Clock
+
+$i = 1
+While ($i -le $sync_attempt_min -or ($i -le $sync_attempt_max -and !$global:sync_corrected)) {
+    Write-Host("`r" + 'Synchronizing...({0}/{1})' -f $i,$sync_attempt_min) -NoNewline
+    Sync-Clock
+    $i++
+    Start-Sleep -Milliseconds $sync_delay
+}
+Write-Host("`r" + 'Synced to', $global:server_clock)
+
+While ($true) {
+    Write-Host("`r" + ($server_clock.AddTicks($stopwatch.ElapsedTicks)).ToString("HH:mm:ss:ff")) -NoNewline
+    Start-Sleep -Milliseconds 20
+}
---
+++ Server Clock v2.ps1
@@ -0,0 +1,125 @@
+﻿# Set Target Server
+$global:uri = 'https://time.is'
+
+# Set Variables
+$global:stopwatch_master = New-Object System.Diagnostics.Stopwatch
+$global:UserAgent = ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
+
+$clock_pool = [System.Collections.ArrayList]@()
+$pool_size = 20
+$generation_delay = 250
+
+
+class ServerClock {
+    [System.Diagnostics.Stopwatch]$stopwatch = [System.Diagnostics.Stopwatch]::new()
+    [DateTime]$server_clock
+    [DateTime]$delta
+    [Boolean]$invalid = $false
+
+    ServerClock() {
+        $r = curl.exe -s -I -L -X HEAD $global:uri -A $global:UserAgent -w "%{time_pretransfer}\n%{time_starttransfer}\n%{time_total}"
+        $this.stopwatch.Start()
+        if ($r.Length -le 3) {
+            # Write-Host('Failed to reach!')
+            $this.stopwatch.Stop()
+            $this.invalid = $true
+        } else {
+            # Get "Date" in HTTP response header / Start backward in case of redirections
+            For ($i=$r.Length-1;$i -ge 0;$i--) {
+                if ($r[$i] -match "^Date:") {
+                    $this.server_clock = [DateTime]$r[$i].Split(',')[1]
+                    break
+                }
+            }
+
+            # Get cURL (time_total - time_starttransfer) + (time_starttransfer - time_pretransfer)/2 in tick
+            $time_to_respond = [int64](([decimal]$r[-1] - [decimal]$r[-2])*10000000) + [int64](([decimal]$r[-2] - [decimal]$r[-3])*5000000)
+        
+            # Roughly compensate for the time-to-respond
+            $this.server_clock = $this.server_clock.AddTicks($time_to_respond)
+
+            # Get Delta
+            $this.delta = $this.GetClock().AddTicks(-$global:stopwatch_master.ElapsedTicks)
+        }
+    }
+
+
+    [DateTime]GetDelta() {
+        return $this.delta
+    }
+
+
+    [DateTime]GetClock() {
+        return $this.server_clock.AddTicks($this.stopwatch.ElapsedTicks)
+    }
+}
+
+# Resize Console Window
+$pswindow = (Get-Host).ui.rawui
+$newsize = $pswindow.windowsize
+$newsize.height = 5
+$newsize.width = 38
+$pswindow.windowsize = $newsize
+$newsize = $pswindow.buffersize
+$newsize.height = 5
+$newsize.width = 38
+$pswindow.buffersize = $newsize
+
+
+Write-Host("`n")
+$global:stopwatch_master.Start()
+$i = 1
+While ($i -le $pool_size) {
+    Write-Host("`r" + ' Generating a pool of clocks...({0}/{1})' -f $i,$pool_size) -NoNewline
+    $clock_pool.Add([ServerClock]::new()) |Out-Null
+    if ($clock_pool[-1].invalid) {$clock_pool.RemoveAt($i-1)}
+    $i++
+    Start-Sleep -Milliseconds $generation_delay
+}
+$global:stopwatch_master.Stop()
+
+
+Clear-Host
+
+
+# Process of finding the best guess, removing outliers
+# Basically, it is finding a one-second timeframe with the most results
+
+$i = 0
+$candidate_done = $false
+$candidate = [System.Collections.ArrayList]@()
+$clock_pool = $clock_pool |Sort -Property delta -Descending
+
+While ($i -lt $clock_pool.Count -and !$candidate_done) {
+    For ($j=$clock_pool.Count-1;$j -ge 0;$j--) {
+        if ($clock_pool[$i].Delta.Ticks - $clock_pool[$j].Delta.Ticks -lt 10000000) {
+            if ($j -eq $clock_pool.Count-1) {$candidate_done = $true}
+            $cc = $j-$i
+            $candidate.Add(($cc,$i))| Out-Null
+            break
+        }
+    }
+    $i++
+}
+
+$candidate = $candidate |Sort -Descending
+$the_clock = $clock_pool[$candidate[0][1]]
+
+
+Write-Host('Synced to', $the_clock.server_clock)
+Write-Host('')
+
+$newsize = $pswindow.windowsize
+$newsize.width = 33
+$pswindow.windowsize = $newsize
+$newsize = $pswindow.buffersize
+$newsize.width = 33
+$pswindow.buffersize = $newsize
+
+
+While ($true) {
+    Write-Host("`r" + " "*8 + ($the_clock.GetClock()).ToString("hh:mm:ss:ff tt")) -NoNewline
+    Start-Sleep -Milliseconds 20
+}
+
+
